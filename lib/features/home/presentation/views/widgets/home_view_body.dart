e41d827471af89f8d7f3b94997f3e13e36bf4314
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-24 00:16:58 +0200

    changed order of items

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,12 +1,10 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:master_pay/core/utility/size_config.dart';
-import 'package:master_pay/core/utility/styles.dart';
 
 import '../../../../../core/widgets/title_with_more.dart';
-import 'home_header_section.dart';
-import 'home_wallet_section.dart';
+import 'home_black_part_of_view.dart';
+import 'home_service_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -32,20 +30,21 @@ class HomeViewBody extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     HomeServiceItem(
+                      svgUrlImg: 'lib/assets/icons/express.svg',
+                      title: 'Shop',
+                    ),
+                    HomeServiceItem(
+                      svgUrlImg: 'lib/assets/icons/master-pay.svg',
+                      title: 'MasterPay',
+                    ),
+
+                    HomeServiceItem(
                       svgUrlImg: 'lib/assets/icons/pay-link.svg',
                       title: 'Pay Link',
                     ),
                     HomeServiceItem(
                       svgUrlImg: 'lib/assets/icons/meta-ads.svg',
                       title: 'Meta ads',
-                    ),
-                    HomeServiceItem(
-                      svgUrlImg: 'lib/assets/icons/master-pay.svg',
-                      title: 'MasterPay',
-                    ),
-                    HomeServiceItem(
-                      svgUrlImg: 'lib/assets/icons/express.svg',
-                      title: 'express',
                     ),
                   ],
                 ),
@@ -56,8 +55,13 @@ class HomeViewBody extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     HomeServiceItem(
-                      svgUrlImg: 'lib/assets/icons/master-tap.svg',
-                      title: 'MasterTAP',
+                      svgUrlImg: 'lib/assets/icons/offers.svg',
+                      title: 'Offers',
+                      isSoon: true,
+                    ),
+                    HomeServiceItem(
+                      svgUrlImg: 'lib/assets/icons/offers.svg',
+                      title: 'Flay',
                       isSoon: true,
                     ),
                     HomeServiceItem(
@@ -66,13 +70,8 @@ class HomeViewBody extends StatelessWidget {
                       isSoon: true,
                     ),
                     HomeServiceItem(
-                      svgUrlImg: 'lib/assets/icons/offers.svg',
-                      title: 'Offers',
-                      isSoon: true,
-                    ),
-                    HomeServiceItem(
-                      svgUrlImg: 'lib/assets/icons/offers.svg',
-                      title: 'Flay',
+                      svgUrlImg: 'lib/assets/icons/master-tap.svg',
+                      title: 'MasterTAP',
                       isSoon: true,
                     ),
                   ],
@@ -86,76 +85,4 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
-class HomeServiceItem extends StatelessWidget {
-  final String title;
-  final String svgUrlImg;
-  final bool isSoon;
 
-  const HomeServiceItem({
-    super.key,
-    required this.title,
-    required this.svgUrlImg,
-    this.isSoon = false,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        isSoon ? Container(
-            padding: const EdgeInsets.symmetric(horizontal: 4,vertical: 2),
-            decoration: BoxDecoration(
-              color: const Color(0xffF13131),
-              borderRadius: BorderRadius.circular(16)
-            ),
-            child: Text('قريبا',style: Styles.style12().copyWith(fontWeight: FontWeight.bold),)) : const SizedBox(),
-        const SizedBox(height: 2,),
-        Container(
-          width: 48,
-          height: 48,
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(color: const Color(0xffEDEDED))),
-          child: SvgPicture.asset(
-            svgUrlImg,
-            width: 20,
-            height: 20,
-          ),
-        ),
-        Text(
-          title,
-          style: Styles.style12().copyWith(
-              fontFamily: Styles.englishFontFamily,
-              color: const Color(0xff838383),
-              fontWeight: FontWeight.w500),
-        ),
-      ],
-    );
-  }
-}
-
-class HomeBlackPartOfView extends StatelessWidget {
-  const HomeBlackPartOfView({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(left: 28, right: 28, top: 48),
-      width: SizeConfig.screenWidth,
-      height: SizeConfig.screenHeight * 0.40,
-      decoration: const BoxDecoration(color: Color(0xff121212)),
-      child: const Column(
-        children: [
-          HomeHeaderSection(),
-          SizedBox(
-            height: 32,
-          ),
-          HomeWalletSection(),
-        ],
-      ),
-    );
-  }
-}
